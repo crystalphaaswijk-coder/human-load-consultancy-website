0d30341862d0b5ad8be6bd9f33750c517de453d8
human load consultancy <crystal.p.haaswijk@gmail.com>
2026-03-09 01:41:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const midLayer = document.querySelector(".hero-mid img");
+const heroBack = document.querySelector(".hero-back");
+const heroMid = document.querySelector(".hero-mid img");
+const heroFront = document.querySelector(".hero-front");
 
-window.addEventListener("scroll",()=>{
+window.addEventListener("scroll", () => {
 
 const scroll = window.scrollY;
 
-midLayer.style.transform =
-`translateY(${scroll * 0.45}px) scale(1.15)`;
+if(heroBack){
+heroBack.style.transform = `translateY(${scroll * 0.15}px)`;
+}
+
+if(heroMid){
+heroMid.style.transform = `translateY(${scroll * 0.35}px) scale(1.1)`;
+}
+
+if(heroFront){
+heroFront.style.transform = `translateY(${scroll * 0.05}px)`;
+}
 
 });
