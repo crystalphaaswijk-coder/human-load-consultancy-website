5f6ef241312e067a7b3dbee7ff0f0c84d033543f
human load consultancy <crystal.p.haaswijk@gmail.com>
2026-03-09 01:51:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,44 @@ requestAnimationFrame(smoothScroll);
 }
 
 smoothScroll();
+
+const heroBack = document.querySelector(".hero-back");
+const heroMid = document.querySelector(".hero-mid img");
+const heroFront = document.querySelector(".hero-front");
+
+window.addEventListener("scroll", () => {
+
+let scroll = window.scrollY;
+
+if(heroBack){
+heroBack.style.transform =
+`translateY(${scroll * 0.2}px)`;
+}
+
+if(heroMid){
+heroMid.style.transform =
+`translateY(${scroll * 0.35}px) scale(1.1)`;
+}
+
+if(heroFront){
+heroFront.style.transform =
+`translateY(${scroll * 0.1}px)`;
+}
+
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("in-view");
+}
+});
+},{
+threshold:0.2
+});
+
+reveals.forEach(el=>{
+observer.observe(el);
+});
