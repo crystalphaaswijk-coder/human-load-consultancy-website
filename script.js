668744b24f7620d2f5269b307e73003ac72f96af
human load consultancy <crystal.p.haaswijk@gmail.com>
2026-03-13 21:33:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,20 @@ Math.max((window.innerHeight - rect.top) / height, 0),
 progress.style.height = scrolled * 100 + "%";
 
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if(elementTop < windowHeight - 80){
+      el.classList.add("visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
